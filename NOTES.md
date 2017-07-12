118e9310a43c81b0ae25ddadd1553f8c8e46ff04
Matt Payne <payne@mattpayne.org>
2017-07-12 02:30:00 +0000

    it compiles

---
+++ NOTES.md
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/43677629/invalid-host-header-in-when-running-angular-cli-development-server-c9-io
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import {Inject, Injectable, OpaqueToken} from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+export class MyHammerConfig extends HammerGestureConfig  {
+  overrides = <any>{
+      'swipe': {velocity: 0.4, threshold: 20} // override default settings
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+	provide: HAMMER_GESTURE_CONFIG, 
+        useClass: MyHammerConfig 
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
